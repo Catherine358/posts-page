62fe99b6b97bba3e90be87d92329d443b75a049c
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2025-10-07 16:54:51 +0200

    added toggle favoirites to PostItem.tsx

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,5 +1,6 @@
 import type {Post} from "../types/post.ts";
 import type {User} from "../types/user.ts";
+import {useStorePosts} from "../store/useStorePosts.ts";
 
 interface PostItemProps {
     data: {
@@ -11,6 +12,9 @@ interface PostItemProps {
 
 export default function PostItem({ data, onFilterByUser }: PostItemProps) {
     const { post, user } = data;
+    const { favPosts, toggleFavPosts } = useStorePosts();
+
+    const isFavorite = favPosts.includes(post.id);
 
   return (
     <article className="bg-white border border-gray-200 rounded-lg shadow-sm p-4 hover:shadow-md transition-shadow duration-200">
@@ -26,9 +30,10 @@ export default function PostItem({ data, onFilterByUser }: PostItemProps) {
                 </button>
             )}
             <button
+                onClick={() => toggleFavPosts(post.id)}
                 className="text-xl text-gray-400 hover:text-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-300 rounded"
             >
-                {'☆'}
+                {isFavorite ? '★' : '☆'}
             </button>
         </div>
     </article>
